224d76325d12cfc65bdc033f092c455ff064ad5b
Courtney Bryce Hilton <courtney.bryce.hilton@gmail.com>
2018-05-18 14:39:28 +1000

    Basic outline of experiment

--- Word_Meter_Congruency.py
+++ Word_Meter_Congruency.py
@@ -1,3 +1,50 @@
+#!/usr/bin/env python2
+
+################################################
+################# Imports ######################
+################################################
 
 
-from psychopy import core, visual, logging, gui, event, parallel, prefs, data
+GlobalClock = core.Clock() # Track time since experiment starts
+
+
+################################################
+############### Basic checks ###################
+################################################
+# Setting of sound driver stuff?
+# check relative paths correct
+# Check port info IF EEG
+    #port = parallel.ParallelPort(address=0xd050) 
+    #port.setData(0)    
+
+################################################
+####### Collect experiment session info ########
+################################################
+#Exp name
+#Define experiment info
+#prompt user for info
+# Add DateTime stamp
+# Create filename
+
+################################################
+################ Setup logfile #################
+################################################
+
+
+################################################
+################# Variables ####################
+################################################
+# setup Auditory sounds
+# setup window
+# setup and load instructions text stimuli
+# setup and load experimental text stimuli
+
+
+################################################
+########## Trial list construction #############
+################################################
+
+
+################################################
+############## Run experiment ##################
+################################################
