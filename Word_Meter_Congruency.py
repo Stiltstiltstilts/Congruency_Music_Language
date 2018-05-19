b3367be2abe343cd51a828c15ff5b93118708e7d
Courtney Bryce Hilton <courtney.bryce.hilton@gmail.com>
2018-05-19 21:55:37 +1000

    Basic stuff and setting python virtualenv

--- Word_Meter_Congruency.py
+++ Word_Meter_Congruency.py
@@ -3,7 +3,14 @@
 ################################################
 ################# Imports ######################
 ################################################
-
+from psychopy import core, visual, logging, gui, event, parallel, prefs, data
+import sounddevice as sd
+import soundfile as sf
+prefs.general['audioLib'] = ['sounddevice']
+from numpy.random import random, randint, normal, shuffle
+from psychopy import sound
+import os, sys, itertools  
+#from constants import *
 
 GlobalClock = core.Clock() # Track time since experiment starts
 
@@ -11,9 +18,13 @@ GlobalClock = core.Clock() # Track time since experiment starts
 ################################################
 ############### Basic checks ###################
 ################################################
-# Setting of sound driver stuff?
-# check relative paths correct
-# Check port info IF EEG
+### Setting of sound driver stuff?
+sd.default.device = 12 # Audigy ASIO driver for 16bit 48000HZ
+sd.default.latency = ('low','low')
+### check relative paths correct
+print(os.path.join(os.path.dirname(__file__), '..'))
+print(os.path.abspath(os.path.dirname(__file__)))
+### Check port info IF EEG
     #port = parallel.ParallelPort(address=0xd050) 
     #port.setData(0)    
 
