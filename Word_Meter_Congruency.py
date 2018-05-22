d2a5a93f56656b2df9305ee37e0016b9230bd50f
Courtney Bryce Hilton <courtney.bryce.hilton@gmail.com>
2018-05-22 10:24:31 +1000

    Putting basics in place

--- Word_Meter_Congruency.py
+++ Word_Meter_Congruency.py
@@ -1,9 +1,11 @@
+#!/Users/Stilts/PsychoPyBuild/bin/python 
+
 ################################################
 ################# Imports ######################
 ################################################
 from psychopy import core, visual, logging, gui, event, parallel, prefs, data
-import sounddevice as sd
 import soundfile as sf
+import sounddevice as sd
 prefs.general['audioLib'] = ['sounddevice']
 from numpy.random import random, randint, normal, shuffle
 from psychopy import sound
@@ -20,31 +22,42 @@ GlobalClock = core.Clock() # Track time since experiment starts
 sd.default.device = 12 # Audigy ASIO driver for 16bit 48000HZ
 sd.default.latency = ('low','low')
 ### check relative paths correct
-print(os.path.join(os.path.dirname(__file__), '..'))
-print(os.path.abspath(os.path.dirname(__file__)))
-### Check port info IF EEG
-    #port = parallel.ParallelPort(address=0xd050) 
-    #port.setData(0)    
+_thisDir = os.path.abspath(os.path.dirname(__file__)))
+os.chdir(_thisDir)
 
 ################################################
 ####### Collect experiment session info ########
 ################################################
 #Exp name
+expName = 'MeterSyntax'
 #Define experiment info
-#prompt user for info
-# Add DateTime stamp
-# Create filename
+expInfo = {'session':'001', 'participant':'001', 'order':1, 'handedness':'', 'gender':''}
+dlg = gui.DlgFromDict(dictionary=expInfo, title=expName)
+if dlg.OK == False:
+    core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+# Create filename for data file (absolute path + name)
+filename = _thisDir + os.sep + 'data/{0}'.format(expInfo['participant'])
 
 ################################################
 ################ Setup logfile #################
 ################################################
-
+# save a log file for detail verbose info
+logFile = logging.LogFile(filename+'.log', level=logging.DATA)
+logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
 
 ################################################
 ################# Variables ####################
 ################################################
-# setup Auditory sounds
+####====Auditory Stimuli====####
+beat_stim, sd.default.samplerate = sf.read('pure_tone3.wav')
+beat_inter, sd.default.samplerate = sf.read('tone_interruption.wav')
 # setup window
+win = visual.Window(fullscr=True,
+                monitor='Laptop',
+                units='deg',
+                allowGUI=False)
 # setup and load instructions text stimuli
 # setup and load experimental text stimuli
 
